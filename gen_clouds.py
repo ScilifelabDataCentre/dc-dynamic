6a23d5c2bae36b46dab7e261f65627b851bc70c0
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-02-19 15:33:12 +0100

    Put covid clouds in their own folder

--- gen_clouds.py
+++ gen_clouds.py
@@ -5,5 +5,5 @@ import livewordcloud as lwc
 PATH = os.environ.get("PYTHONPATH")
 CODE_PATH = os.environ.get("CODE_PATH")
 
-lwc.write_file(os.path.join(CODE_PATH, 'output/cloud_titles.png'), lwc.gen_wordcloud(field='title', data_folder=PATH))
-lwc.write_file(os.path.join(CODE_PATH, 'output/cloud_abstracts.png'), lwc.gen_wordcloud(field='abstract', data_folder=PATH))
+lwc.write_file(os.path.join(CODE_PATH, 'output/covid-portal/cloud_titles.png'), lwc.gen_wordcloud(field='title', data_folder=PATH))
+lwc.write_file(os.path.join(CODE_PATH, 'output/covid-portal/cloud_abstracts.png'), lwc.gen_wordcloud(field='abstract', data_folder=PATH))
