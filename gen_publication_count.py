4742234e1d4f40d30233246ca260895ec69ad2ea
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-03-12 11:19:22 +0100

    Generate publication data

---
+++ gen_publication_count.py
@@ -0,0 +1,35 @@
+import json
+
+import pandas
+import requests
+
+# get data
+res = requests.get("https://publications-covid19.scilifelab.se/publications.json")
+txt = res.json()
+df = pandas.json_normalize(txt["publications"])
+df = df[["type", "published"]]
+
+# for now, assume dates with day as 00 are 1st day
+df.replace("-00", "-01", regex=True, inplace=True)
+
+# make the cumulative sum of papers published
+df.published = pandas.to_datetime(df["published"], format="%Y-%m-%d").dt.date
+currdate = pandas.to_datetime("today").date()
+df = df[df["published"] < currdate]
+df.sort_values(by="published", inplace=True)
+df1 = df["published"].value_counts().sort_index().reset_index()
+df1["cumulativecount"] = df1["published"].cumsum()
+
+# find number of papers published in each month
+df["year"] = pandas.DatetimeIndex(df["published"]).year
+df["month"] = pandas.DatetimeIndex(df["published"]).month
+df["index"] = pandas.to_datetime(df[["year", "month"]].assign(day=1))
+df2 = df.groupby(["index"]).size().reset_index(name="Number Added")
+
+outdata = {}
+outdata["dates"] = list(str(val) for val in df1["index"])
+outdata["cumsums"] = list(df1["cumulativecount"])
+outdata["months"] = list(str(val).split()[0] for val in df2["index"])
+outdata["per_month"] = list(df2["Number Added"])
+
+print(json.dumps(outdata))
--- runner.sh
+++ runner.sh
@@ -1,7 +1,7 @@
 export CODE_PATH=/code
 
 PYTHONPATH="$CODE_PATH"/Covid_portal_vis/Wordcloud python "$CODE_PATH"/gen_clouds.py
-
 python "$CODE_PATH"/gen_symptoms_data.py > "$CODE_PATH"/output/covid-portal/symptoms-data.json
+python "$CODE_PATH"/gen_publication_count.py > "$CODE_PATH"/output/covid-portal/publication-counts.json
 
 chown -R 101.101 "$CODE_PATH"/output/*
