48eba38dbb4313ab2802c979f3ac38426e85a5a8
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2021-03-24 12:39:08 +0100

    Add recent publications to daily run

--- runner_daily.sh
+++ runner_daily.sh
@@ -2,5 +2,6 @@ export CODE_PATH=/code
 
 python "$CODE_PATH"/gen_symptoms_data.py > "$CODE_PATH"/output/covid-portal/symptoms-data.json
 python "$CODE_PATH"/gen_publication_count.py > "$CODE_PATH"/output/covid-portal/publication-counts.json
+python "$CODE_PATH"/gen_recent_pub.py > "$CODE_PATH"/output/covid-portal/recent10.json
 
 chown -R 101.101 "$CODE_PATH"/output/*
