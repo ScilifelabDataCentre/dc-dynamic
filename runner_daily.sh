466463c274c9853a3515bad1cebeebf7bc6ffc74
LianeHughes <liane.hughes@scilifelab.uu.se>
2022-02-09 08:38:06 +0100

    Add English version symptoms map to daily runner
    This version of the symptoms map is more accessible (colour and hover features). It should also allow us to bring the symptoms map from the blob server (which is more standard for the portal).

--- runner_daily.sh
+++ runner_daily.sh
@@ -3,7 +3,7 @@ export CODE_PATH=/code
 python "$CODE_PATH"/gen_symptoms_data.py > "$CODE_PATH"/output/covid-portal-symptoms-data.json
 python "$CODE_PATH"/gen_publication_count.py > "$CODE_PATH"/output/covid-portal-publication-counts.json
 python "$CODE_PATH"/gen_recent_pub.py > "$CODE_PATH"/output/covid-portal-recent10.json
-python "$CODE_PATH"/Covid_portal_vis/map/symptoms_map_English.py > "$CODE_PATH"/output/Symptoms_map_English.json
+PYTHONPATH="$CODE_PATH"/Covid_portal_vis/map python symptoms_map_English.py > "$CODE_PATH"/output/symptoms_map_english.json
 
 # Upload generated files
 for filename in $(ls $CODE_PATH/output) ; do
