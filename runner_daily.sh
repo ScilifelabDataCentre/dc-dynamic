2accb6450b8aad4e25a602bcd464d81569723dc3
LianeHughes <liane.hughes@scilifelab.uu.se>
2022-02-15 06:23:50 +0100

    Add reference to Swedish version of symptoms map

--- runner_daily.sh
+++ runner_daily.sh
@@ -4,6 +4,7 @@ python "$CODE_PATH"/gen_symptoms_data.py > "$CODE_PATH"/output/covid-portal-symp
 python "$CODE_PATH"/gen_publication_count.py > "$CODE_PATH"/output/covid-portal-publication-counts.json
 python "$CODE_PATH"/gen_recent_pub.py > "$CODE_PATH"/output/covid-portal-recent10.json
 PYTHONPATH="$CODE_PATH"/Covid_portal_vis/map python "$CODE_PATH"/Covid_portal_vis/map/symptoms_map_English.py > "$CODE_PATH"/output/symptoms_map_english.json
+PYTHONPATH="$CODE_PATH"/Covid_portal_vis/map python "$CODE_PATH"/Covid_portal_vis/map/symptoms_map_Swedish.py > "$CODE_PATH"/output/symptoms_map_swedish.json
 
 # Upload generated files
 for filename in $(ls $CODE_PATH/output) ; do
