d7f9ed48d1e91f500a1ada11754d77414a58ea17
LianeHughes <liane.hughes@scilifelab.uu.se>
2022-02-24 09:35:51 +0100

    Update path to show new vis repo name

--- runner_daily.sh
+++ runner_daily.sh
@@ -3,8 +3,8 @@ export CODE_PATH=/code
 python "$CODE_PATH"/gen_symptoms_data.py > "$CODE_PATH"/output/covid-portal-symptoms-data.json
 python "$CODE_PATH"/gen_publication_count.py > "$CODE_PATH"/output/covid-portal-publication-counts.json
 python "$CODE_PATH"/gen_recent_pub.py > "$CODE_PATH"/output/covid-portal-recent10.json
-PYTHONPATH="$CODE_PATH"/Covid_portal_vis/map python "$CODE_PATH"/Covid_portal_vis/map/symptoms_map_English.py > "$CODE_PATH"/output/symptoms_map_english.json
-PYTHONPATH="$CODE_PATH"/Covid_portal_vis/map python "$CODE_PATH"/Covid_portal_vis/map/symptoms_map_Swedish.py > "$CODE_PATH"/output/symptoms_map_swedish.json
+PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/map python "$CODE_PATH"/covid-portal-visualisations/map/symptoms_map_English.py > "$CODE_PATH"/output/symptoms_map_english.json
+PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/map python "$CODE_PATH"/covid-portal-visualisations/map/symptoms_map_Swedish.py > "$CODE_PATH"/output/symptoms_map_swedish.json
 
 # Upload generated files
 for filename in $(ls $CODE_PATH/output) ; do
