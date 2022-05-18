47b8049f605bcae15d460c293bce6c9be6bac2bf
Senthilkumar Panneerselvam <senthilkumar.panneerselvam@scilifelab.se>
2022-05-18 09:57:52 +0200

    Update weekly runner to update vaccine related blobs for covid portal

--- runner_weekly.sh
+++ runner_weekly.sh
@@ -6,3 +6,19 @@ PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/Wordcloud python "$CODE_PATH
 for filename in $(ls $CODE_PATH/output) ; do
     curl "https://blobserver.dckube.scilifelab.se/blob/$filename" -H "x-accesskey: $ACCESS_KEY" --upload-file "output/$filename"
 done
+
+# Auto update of vaccine page in covid portal
+
+cd $CODE_PATH
+
+export VACCINE_SCRIPTS_PATH="$CODE_PATH"/covid-portal-visualisations/Vaccine_page
+
+python "$VACCINE_SCRIPTS_PATH"/vaccine_indicator_barchart.py
+python "$VACCINE_SCRIPTS_PATH"/vaccine_livetext.py
+python "$VACCINE_SCRIPTS_PATH"/vaccine_timeseries_barchart.py
+python "$VACCINE_SCRIPTS_PATH"/vaccine_maps_population.py
+python "$VACCINE_SCRIPTS_PATH"/vaccine_heatmaps.py
+
+for filename in $(ls $CODE_PATH/vaccine_plots) ; do
+    curl "https://blobserver.dckube.scilifelab.se/blob/$filename" -H "x-accesskey: $ACCESS_KEY" --upload-file "$CODE_PATH/vaccine_plots/$filename"
+done
