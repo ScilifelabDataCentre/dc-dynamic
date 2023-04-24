929c89fc1f8b2dc553648b3a3b9d59ac5d6240c8
LianeHughes <liane.hughes@scilifelab.uu.se>
2023-04-24 07:34:17 +0200

    Add COVID graph for gothenburg group
    Blob set up in blob server (see https://blobserver.dckube.scilifelab.se/blob/wastewater_gothenburg.json/info). Needs a regular update

--- runner_every10mins.sh
+++ runner_every10mins.sh
@@ -4,6 +4,7 @@ PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/wastewater python "$CODE_PAT
 PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/wastewater python "$CODE_PATH"/covid-portal-visualisations/wastewater/combined_slu_regular.py > "$CODE_PATH"/output/wastewater_combined_slu_regular.json
 PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/wastewater python "$CODE_PATH"/covid-portal-visualisations/wastewater/combined_stockholm_logyaxis.py > "$CODE_PATH"/output/wastewater_stockholm_logyaxis.json
 PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/wastewater python "$CODE_PATH"/covid-portal-visualisations/wastewater/combined_stockholm_regular.py > "$CODE_PATH"/output/wastewater_combined_stockholm.json
+PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/wastewater python "$CODE_PATH"/covid-portal-visualisations/wastewater/gothenburg_covid.py > "$CODE_PATH"/output/wastewater_gothenburg.json
 
 # Upload generated files
 for filename in $(ls $CODE_PATH/output) ; do
