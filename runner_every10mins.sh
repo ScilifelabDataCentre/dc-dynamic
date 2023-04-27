f8baa0426534e1ebb05e44666ea3a37fbc4eb0c9
LianeHughes <liane.hughes@scilifelab.uu.se>
2023-04-27 11:48:21 +0200

    add Malmö COVID wastewater graph to cron

--- runner_every10mins.sh
+++ runner_every10mins.sh
@@ -5,7 +5,7 @@ PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/wastewater python "$CODE_PAT
 PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/wastewater python "$CODE_PATH"/covid-portal-visualisations/wastewater/combined_stockholm_logyaxis.py > "$CODE_PATH"/output/wastewater_stockholm_logyaxis.json
 PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/wastewater python "$CODE_PATH"/covid-portal-visualisations/wastewater/combined_stockholm_regular.py > "$CODE_PATH"/output/wastewater_combined_stockholm.json
 PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/wastewater python "$CODE_PATH"/covid-portal-visualisations/wastewater/gothenburg_covid.py > "$CODE_PATH"/output/wastewater_gothenburg.json
-
+PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/wastewater python "$CODE_PATH"/covid-portal-visualisations/wastewater/quant_malmö_kthplot.py > "$CODE_PATH"/output/wastewater_kthmalmö.json
 # Upload generated files
 for filename in $(ls $CODE_PATH/output) ; do
     curl "https://blobserver.dckube.scilifelab.se/blob/$filename" -H "x-accesskey: $ACCESS_KEY" --upload-file "output/$filename"
