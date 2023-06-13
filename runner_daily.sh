7f139dab383bd38fdc32618212089143ee125aa3
Senthilkumar Panneerselvam <senthilkumar.panneerselvam@scilifelab.se>
2023-06-13 08:25:55 +0200

    Re-arrange and remove unwanted visualisation scripts

--- runner_daily.sh
+++ runner_daily.sh
@@ -1,14 +1,3 @@
 export CODE_PATH=/code
 
-python "$CODE_PATH"/gen_symptoms_data.py > "$CODE_PATH"/output/covid-portal-symptoms-data.json
-python "$CODE_PATH"/gen_publication_count.py > "$CODE_PATH"/output/covid-portal-publication-counts.json
-python "$CODE_PATH"/gen_recent_pub.py > "$CODE_PATH"/output/covid-portal-recent10.json
-PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/map python "$CODE_PATH"/covid-portal-visualisations/map/symptoms_map_English.py > "$CODE_PATH"/output/symptoms_map_english.json
-PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/map python "$CODE_PATH"/covid-portal-visualisations/map/symptoms_map_Swedish.py > "$CODE_PATH"/output/symptoms_map_swedish.json
-
-# Upload generated files
-for filename in $(ls $CODE_PATH/output) ; do
-    curl "https://blobserver.dc.scilifelab.se/blob/$filename" -H "x-accesskey: $ACCESS_KEY" --upload-file "output/$filename"
-done
-
 python "$CODE_PATH"/slack_figshare.py
--- runner_weekly.sh
+++ runner_weekly.sh
@@ -2,14 +2,9 @@ export CODE_PATH=/code
 
 PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/Wordcloud python "$CODE_PATH"/gen_clouds.py
 
-# Auto update of vaccine page in covid portal
-export VACCINE_SCRIPTS_PATH="$CODE_PATH"/covid-portal-visualisations/Vaccine_page
-
-python "$VACCINE_SCRIPTS_PATH"/vaccine_indicator_barchart.py --output-dir $CODE_PATH/output
-python "$VACCINE_SCRIPTS_PATH"/vaccine_livetext.py --output-dir $CODE_PATH/output
-python "$VACCINE_SCRIPTS_PATH"/vaccine_timeseries_barchart.py --output-dir $CODE_PATH/output
-python "$VACCINE_SCRIPTS_PATH"/vaccine_maps_population.py --output-dir $CODE_PATH/output
-python "$VACCINE_SCRIPTS_PATH"/vaccine_heatmaps.py --output-dir $CODE_PATH/output
+# Publication related updates
+python "$CODE_PATH"/gen_publication_count.py > "$CODE_PATH"/output/covid-portal-publication-counts.json
+python "$CODE_PATH"/gen_recent_pub.py > "$CODE_PATH"/output/covid-portal-recent10.json
 
 # Upload generated files
 for filename in $(ls $CODE_PATH/output) ; do
