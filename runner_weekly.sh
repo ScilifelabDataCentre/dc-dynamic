e0c86976bfac4d38e3686c33508996e44b628656
LianeHughes <68756672+LianeHughes@users.noreply.github.com>
2023-10-23 09:18:27 +0200

    update publications graph
    Also remove KTH graphs from runner (not updating for now)

--- runner_weekly.sh
+++ runner_weekly.sh
@@ -3,7 +3,8 @@ export CODE_PATH=/code
 PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/Wordcloud python "$CODE_PATH"/gen_clouds.py
 
 # Publication related updates
-python "$CODE_PATH"/gen_publication_count.py > "$CODE_PATH"/output/covid-portal-publication-counts.json
+PYTHONPATH="$CODE_PATH"/covid-portal-visualisations/Count_publications python "$CODE_PATH"/covid-portal-visualisations/Count_publications/count_publications.py > "$CODE_PATH"/output/COVID_publication_count.json
+#python "$CODE_PATH"/gen_publication_count.py > "$CODE_PATH"/output/covid-portal-publication-counts.json
 python "$CODE_PATH"/gen_recent_pub.py > "$CODE_PATH"/output/covid-portal-recent10.json
 
 # Upload generated files
