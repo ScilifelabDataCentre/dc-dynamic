eebdf409d60742cf4b61efba1567a7ec8d01dde5
Senthilkumar Panneerselvam <senthilkumar.panneerselvam@scilifelab.se>
2024-04-15 09:56:10 +0200

    Add EBI index updater

--- runner_daily.sh
+++ runner_daily.sh
@@ -1,3 +1,5 @@
 export CODE_PATH=/code
 
 python "$CODE_PATH"/slack_figshare.py
+
+PYTHONPATH="$CODE_PATH"/pathogens-portal-scripts/EBI_indexing python "$CODE_PATH"/pathogens-portal-scripts/EBI_indexing/update_index_json.py
