2d746672ff7c43377d6616c43a51cf3ebae8c675
Abdullah Aziz <mister.abdullah.aziz@gmail.com>
2024-10-03 17:39:43 +0200

    update 10 minutes runner for clicmicro visualization

--- runner_every10mins.sh
+++ runner_every10mins.sh
@@ -11,6 +11,10 @@ PYTHONPATH="$CODE_PATH"/pathogens-portal-visualisations/wastewater python "$CODE
 PYTHONPATH="$CODE_PATH"/pathogens-portal-visualisations/serology python "$CODE_PATH"/pathogens-portal-visualisations/serology/weekly-serology-tests.py > "$CODE_PATH"/output/weekly_serology_tests.json
 PYTHONPATH="$CODE_PATH"/pathogens-portal-visualisations/serology python "$CODE_PATH"/pathogens-portal-visualisations/serology/cumulative-serology-tests.py > "$CODE_PATH"/output/cumulative_serology_tests.json
 
+# ClinMicro statistics is based on these files under /pathogens-portal-visualisations/ClinMicro:
+PYTHONPATH="$CODE_PATH"/pathogens-portal-visualisations/ClinMicro python "$CODE_PATH"/pathogens-portal-visualisations/ClinMicro/lineage-plotting-one.py > "$CODE_PATH"/output/lineage_one_wholetime.json
+PYTHONPATH="$CODE_PATH"/pathogens-portal-visualisations/ClinMicro python "$CODE_PATH"/pathogens-portal-visualisations/ClinMicro/lineage-plotting-four.py > "$CODE_PATH"/output/lineage_four_recent.json
+PYTHONPATH="$CODE_PATH"/pathogens-portal-visualisations/ClinMicro python "$CODE_PATH"/pathogens-portal-visualisations/ClinMicro/lineage-plotting-five.py > "$CODE_PATH"/output/lineage_five_recent.json
 
 # Upload generated files
 for filename in $(ls $CODE_PATH/output) ; do
