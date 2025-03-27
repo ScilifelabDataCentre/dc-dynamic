7806f196e25dc086f6a5ff8d798914d18542c85b
Abdullah Aziz <mister.abdullah.aziz@gmail.com>
2025-03-27 13:16:43 +0100

    Refactors daily Slack runner, removs hourly runner

--- slack_runner_daily.sh
+++ slack_runner_daily.sh
@@ -2,3 +2,12 @@ export CODE_PATH=/code
 
 # Feed SciLifeLab figshare data to Slack
 python "$CODE_PATH"/slack_figshare.py
+
+# Post items from SciLifeLab feed to Slack channel
+python "$CODE_PATH"/slack_feeds.py $CODE_PATH/output
+
+
+# Upload generated files
+for filename in $(ls $CODE_PATH/output) ; do
+    curl "https://blobserver.dc.scilifelab.se/blob/$filename" -H "x-accesskey: $ACCESS_KEY" --upload-file "output/$filename"
+done
